1f31d1b34c84ccda6331b012d70ccd095325bdf3
oleg <olegp306@gmail.com>
2018-10-04 12:55:47 +0300

    implement navigator

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import  {Text, View} from 'react-native';
-// import { StackNavigator } from 'react-navigation'
+import  {Text, View, Button,KeyboardAvoidingView} from 'react-native';
+import { createStackNavigator } from 'react-navigation'
 
 import LoginComponent from './app/components/LoginComponent.js';
 import RequestListComponent from './app/components/RequestListComponent.js';
 import NorifyOfficeComponent from './app/components/NorifyOfficeComponent.js';
-import  BarCodeScannerScreen from './app/components/BarCodeScannerScreen.js';
-import  AddCommentScreen from './app/components/AddCommentScreen.js';
+import BarCodeScannerScreen from './app/components/BarCodeScannerScreen.js';
+import AddCommentScreen from './app/components/AddCommentScreen.js';
+import HomeScreen from './app/components/HomeScreen.js'
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Login: LoginComponent,
+    RequestList: RequestListComponent,
+    AddComment: AddCommentScreen,
+    BarCodeScanner: BarCodeScannerScreen,
+    NorifyOffice: NorifyOfficeComponent,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
 
 
 export default class HelloWorld extends Component{
   render(){
-    return(
-      <View>
-        {/* <LoginComponent /> */}
-        <RequestListComponent />
-        {/* <NorifyOfficeComponent /> */}
-        {/* <BarCodeScannerScreen /> */}
-        {/* <AddCommentScreen /> */}
-      </View>
+
+    return (
+      <RootStack />
     )
+    
 
   }
 }
