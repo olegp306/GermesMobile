20ea42fe9b253f32b03d4415e030c707b0b06237
oleg <olegp306@gmail.com>
2018-10-05 14:51:14 +0300

    Переработал навигацию. Убоал хедеры у форм входа и начальной формы. Добавил хедеры для отстальных экранов

--- App.js
+++ App.js
@@ -1,30 +1,87 @@
 import React, { Component } from 'react';
-import  {Text, View, Button,KeyboardAvoidingView} from 'react-native';
+import  {Text, View, Button,KeyboardAvoidingView,StyleSheet} from 'react-native';
 import { createStackNavigator } from 'react-navigation'
 
-import LoginComponent from './app/components/LoginComponent.js';
-import RequestListScreen from './app/components/RequestListScreen.js';
-import RequestListComponent from './app/components/RequestListComponent.js';
+import HomeScreen from './app/containers/HomeScreen.js';
+import LoginScreen from './app/components/LoginComponent.js';
+import RequestListScreen from './app/containers/RequestListScreen.js';
+import AddCommentScreen from './app/containers/AddCommentScreen.js';
+import BarCodeScannerScreen from './app/containers/BarCodeScannerScreen.js';
 import NorifyOfficeComponent from './app/components/NorifyOfficeComponent.js';
-import BarCodeScannerScreen from './app/components/BarCodeScannerScreen.js';
-import AddCommentScreen from './app/components/AddCommentScreen.js';
-import HomeScreen from './app/components/HomeScreen.js'
 import CheckBoxExampleComponent from './app/components/CheckBoxExampleComponent.js'
 
-CheckBoxExampleComponent
+const styles = StyleSheet.create({
+  back: { 
+      backgroundColor: '#C9C8C7'
+  }, 
+  title: { color: '#53565A', fontSize: 18 }
+})
 
 const RootStack = createStackNavigator(
   {
-    Home: HomeScreen,
-    Login: LoginComponent,
-    RequestList: RequestListScreen,
-    AddComment: AddCommentScreen,
-    BarCodeScanner: BarCodeScannerScreen,
-    NorifyOffice: NorifyOfficeComponent,
-    CheckBoxExample: CheckBoxExampleComponent
+    // Home: HomeScreen
+      Home:{
+       screen: HomeScreen,
+       navigationOptions: { header: null }
+    }
+    ,
+    Login: {screen: LoginScreen},
+    // Login: {
+    //   screen: LoginScreen,
+    //   navigationOptions:{
+    //     headerTitle: 'вход'
+    //    }
+   //}
+   //,
+
+    // RequestList: RequestListScreen,
+    RequestList: {
+      screen: RequestListScreen,
+      navigationOptions:{
+        headerTitle: 'Заявки'
+       }
+   },
+
+    // AddComment: AddCommentScreen,
+    AddComment: {
+      screen: AddCommentScreen,
+      navigationOptions:{
+        headerTitle: 'Комментарии по заявке'
+       }
+   },
+
+    // BarCodeScanner: BarCodeScannerScreen,
+    BarCodeScanner: {
+      screen: BarCodeScannerScreen,
+      navigationOptions:{
+        headerTitle: 'Сканирование штрихкодов'
+       }
+   },
+
+    // NorifyOffice: NorifyOfficeComponent,
+    NorifyOffice: {
+      screen: NorifyOfficeComponent,
+      navigationOptions:{
+        headerTitle: 'Уведомить о получении '
+       }
+   },
+
+    // CheckBoxExample: CheckBoxExampleComponent
+    CheckBoxExample: {
+      screen: CheckBoxExampleComponent,
+      navigationOptions:{
+        headerTitle: 'примеры Чекбоксов'
+       }
+   }
   },
   {
     initialRouteName: 'Home',
+
+    navigationOptions: {
+            headerStyle: styles.back,
+            headerTitleStyle: styles.title,
+            headerTintColor: '#53565A'
+        }
   }
 );
 
