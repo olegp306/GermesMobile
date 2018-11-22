0232d18ba0bb29bfdc6ab6a1d992b0fb8ded5fff
oleg <olegp306@gmail.com>
2018-11-22 14:23:56 +0300

    change to only one request? to control all items states

--- app/store/germes/requests/actions.js
+++ app/store/germes/requests/actions.js
@@ -7,16 +7,13 @@ export const REQUESTS_STATUS_CHANGE = 'REQUESTS_STATUS_CHANGE'
 export const REQUESTS_STATUS_CHANGE_SUCCESS = 'REQUESTS_STATUS_CHANGE_SUCCESS'
 export const REQUESTS_STATUS_CHANGE_ERROR = 'REQUESTS_STATUS_CHANGE_ERROR'
 
-
-
 import _keyBy from 'lodash'
 import api from '../../../middleware/api'
 
 export const fetchRequests = () => {
     return (dispatch, getState)=>{        
         //getState можно получить данные из STORE
-        dispatch(startFetchRequests());
-        
+        dispatch(startFetchRequests());        
         const {filter}=getState();
         //console.log("ACTION fetchRequests");
         api.fetchRequests(filter.get('filterDate'),filter.get('filterReceptionId'))
@@ -46,45 +43,3 @@ export const requestsFetchingError = (error) => {
     }
 }
 
-
-
-// export const changeRequestsStatus = () => {
-//     return (dispatch, getState)=>{        
-//         //getState можно получить данные из STORE
-//         dispatch(startRequestsStatusChange());
-        
-//         const {selectedItems}=getState();
-//         //console.log("ACTION fetchRequests");
-//         api.changeRequestsStatus(Object.keys(selectedItems))
-//         .then(data=>dispatch(requestsFetched(data.data)))
-//         .catch(error=>requestsFetchingError(error))
-//     }
- 
-// }
-
-export const startRequestsStatusChange = (items) => {
-    const {selectedItems}=getState();
-
-    for(key in selectedItems)
-    {
-        api.changeRequestsStatus(key)    
-    }
-    //api.changeRequestsStatus(Object.keys(selectedItems))
-    return {
-        type: REQUESTS_STATUS_CHANGE,
-        //payload:api.toAssociativeArray(items,'requestId')
-    }
-}
-export const requestsStatusChanged = (items) => {
-    return {
-        type: REQUESTS_STATUS_CHANGE_SUCCESS,
-        payload: api.toAssociativeArray(items,'requestId')
-    }
-}
-
-export const requestsStatusChangingError = (error) => {
-    return {
-        type: REQUESTS_STATUS_CHANGE_ERROR,
-        payload: error
-    }
-}
