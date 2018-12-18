1bc0cb5a14a00041e0c193514536d05ce5f80970
oleg <olegp306@gmail.com>
2018-12-18 11:39:04 +0300

    add statusPriostanovkaBackgroundColor

--- app/theme/Colors.js
+++ app/theme/Colors.js
@@ -34,6 +34,7 @@ const Colors = {
   warningBackgroundColor :"#FF0000",
   noteBackgroundColor :"#6AAB33",
   lightBackgroundColor :"#B6DB92",
+  statusPriostanovkaBackgroundColor : "#FFCC00"
   
 
   //https://color.adobe.com/ru/create/color-wheel/?base=2&rule=Analogous&selected=0&name=%D0%9C%D0%BE%D1%8F%20%D1%82%D0%B5%D0%BC%D0%B0%20Color&mode=rgb&rgbvalues=0.01568627450980392,0.4588235294117647,0.5686274509803921,0.3545127659571574,0.04549999999999994,0.91,1,0,0,0.91,0.5806126462347856,0.04495929504613476,0.8082137555665213,1,0.050000000000000044&swatchOrder=0,1,2,3,4
