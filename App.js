b42e075f4130258ef2f49a29f603e22dec308e74
oleg <olegp306@gmail.com>
2018-12-27 15:17:14 +0300

    implement new navigation approach with tabs and stack navigation

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import  {StyleSheet} from 'react-native';
-import { createStackNavigator } from 'react-navigation'
-import {createDrawerNavigator } from 'react-navigation'
+import  {StyleSheet, SafeAreaView, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createStackNavigator, createDrawerNavigator, createBottomTabNavigator, createMaterialTopTabNavigator,createMaterialBottomTabNavigator } from 'react-navigation'
 
 import { Provider } from 'react-redux'
 import { store } from './app/store/index.js'
@@ -39,90 +40,104 @@ const styles = StyleSheet.create({
   },
 })
 
-const RootStack = createStackNavigator(
-  {
-    // Home: HomeScreen
-      Home:{
-       screen: HomeScreen,
-       navigationOptions: { header: null }
-    }
-    ,
-    Login: {screen: LoginScreen,  navigationOptions: { header: null }},
-
-    
-    RequestList: {
-      screen: RequestListScreen,
-      
-   },
-
-    AddComment: {
-      screen: AddCommentScreen,
-      navigationOptions:{
-        headerTitle: 'Чат по заявке(МАКЕТ)'
-       }
-   },
-
-    
-   BarcodeScanner: {
-      screen: BarcodeScannerScreen,
-      navigationOptions:{
-        headerTitle: 'Сканирование штрихкодов'
-       }
-   },
-
-  
-    NotifyOffice: {
-      screen: NotifyOfficeScreen,
-      navigationOptions:{
-        headerTitle: 'Уведомить о получении '
-       }
-   },
-
-  
-  },
-  {
-    initialRouteName: 'Login',
-
-    navigationOptions: {
-            headerStyle: styles.back,
-            headerTitleStyle: styles.title,
-            headerTintColor: Colors.darkBackgroundColor
-    }
-  }  
-);
-
-const routes = {
-  Login: {
-    sc: LoginScreen,
-    
-  },
-  AddComment: {
-    screen: AddCommentScreen,
-    
-  },
-  NotifyOffice: {
-    screen: NotifyOfficeScreen,
-    
-  }
-  
-};
-
-const AppNavigator  = createDrawerNavigator({
-  BarcodeScanner:{
-    screen:BarcodeScannerScreen
-  },
-  NotifyOffice:{
-    screen:NotifyOfficeScreen
-  }
-}
-// ,
-//  {
-//   initialRouteName: 'Login',
+const LoginStackNav = createStackNavigator({
+  Login: { screen: LoginScreen,  navigationOptions: { header: null } },    
+  // SignUp: { screen: SignUpScreen },
+  // ForgottenPassword: { screen: ForgottenPasswordScreen, navigationOptions: { title: 'Forgot Password' } }
+// }, 
+// {
+//   headerMode: 'float',
 //   navigationOptions: {
-    
+//     headerStyle: {backgroundColor: 'red'},
+//     title: 'You are not logged in'
+  // }
+})
+
+// const StackNav = createStackNavigator(
+//   {
+//     // Home: HomeScreen
+//     Home: { screen: HomeScreen, navigationOptions: { header: null } },    
+//     Login: { screen: LoginScreen,  navigationOptions: { header: null } },    
+//     RequestList: { screen: RequestListScreen},
+//     AddComment: { screen: AddCommentScreen},
+//     BarcodeScanner: { screen: BarcodeScannerScreen },  
+//     NotifyOffice: { screen: NotifyOfficeScreen },  
 //   },
-// }
+//   {
+//     initialRouteName: 'Login',
+
+//     navigationOptions: {
+//             headerStyle: styles.back,
+//             headerTitleStyle: styles.title,
+//             headerTintColor: Colors.darkBackgroundColor
+//     }
+//   }  
+// );
+
+const TabsNav  = createBottomTabNavigator(
+  {
+    RequestList: { 
+      screen: RequestListScreen,
+      navigationOptions: {
+        tabBarLabel:"Заявки",
+        tabBarIcon: ({ tintColor }) => <Icon name={"library-books"} color={tintColor} />
+      }
+    },
+    BarcodeScanner:{ 
+      screen:BarcodeScannerScreen,
+      navigationOptions: {
+        tabBarLabel:"Сканер",
+        tabBarIcon: ({ tintColor }) => <Icon2 name={"barcode-scan"} color={tintColor} />
+    }
+
+     },
+    NotifyOffice:{ 
+      screen:NotifyOfficeScreen,
+      navigationOptions: {
+        tabBarLabel:"Отправить",
+        tabBarIcon: ({ tintColor }) => <Icon name={"send"} color={tintColor} />
+      }
+    }  
+  }
+  ,
+  {
+    order: ['RequestList','BarcodeScanner','NotifyOffice'],
+    animationEnabled: true,
+  }
+
 );
+
+// const DrawerNav = createDrawerNavigator(
+//   { 
+//     //StackNav : {screen: StackNav },
+//     //TabsNav : {screen: TabsNav },
+//     //Login: { screen: LoginScreen },
+//     AddComment: { screen: AddCommentScreen },
+//     NotifyOffice: { screen: NotifyOfficeScreen }, 
+//   },
+
+//   // {
+//   //   initialRouteName: 'Login',
+
+//   //   // navigationOptions: {
+//   //   //         headerStyle: styles.back,
+//   //   //         headerTitleStyle: styles.title,
+//   //   //         headerTintColor: Colors.darkBackgroundColor
+//   //   // }
+//   // }
+// );
+
+
+const PrimaryNav= createStackNavigator({
+  LoginStack:{screen: LoginStackNav},
+  TabsNav:{screen: TabsNav}
+  },
+  {
+    initialRouteName: 'LoginStack',
+    navigationOptions: { header: null }
+  }
+)
+
 
 
 //Назначение функции connect вытекает из названия: подключи React компонент к Redux store.
@@ -133,7 +148,9 @@ export default class HelloWorld extends Component{
   render(){
     return (
       <Provider store={store}>
-        <RootStack />
+        <View style={{ flex:1 , paddingTop:25,backgroundColor: Colors.navigatorBackgroudDarkColor}}>
+          <PrimaryNav />
+        </View>
         {/* <RequestListScreen /> */}
         {/* <TestComponent /> */}
       </Provider>
