abbdbe811a161497cedf91541dc8840081b6ab18
oleg <olegp306@gmail.com>
2019-01-14 16:12:58 +0300

    add base MessageComponent

--- app/chat/message/MessageComponent.js
+++ app/chat/message/MessageComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class MessageComponent extends Component {
   constructor(props) {
@@ -7,12 +7,58 @@ export default class MessageComponent extends Component {
     this.state = {
     };
   }
+  
 
   render() {
+    const { isMyMessage ,author, text }=this.props.item;
+    //this.props
+    // let messageText=this.state.messageInfo;
+    // let userInfo=this.state.userInfo;
+    // let isMyMessage=this.state.isMyMessage;
+    let isNewMessage=(!this.state.isNewMessage ? false: true);
+    
     return (
-      <View>
-        <Text> MessageComponent </Text>
+      //<View style={isMyMessage ? styles.rightMessageContainer : styles.leftMessageContainer }>
+      <View style={ styles.leftMessageContainer }>
+        <Text style={styles.authorText}>{ author }</Text>
+        <Text style={styles.messageText}>{ text }</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  leftMessageContainer:{
+    borderWidth: 1,
+    backgroundColor: 'white',
+    borderRadius: 7,
+    alignItems:"flex-start",
+    marginTop:10,
+    marginRight:20
+  },
+  rightMessageContainer:{
+    borderWidth: 1,
+    backgroundColor: '#88c9e8',
+    borderRadius: 7,
+    alignItems:"flex-end",
+    marginTop:10,
+    marginLeft:20
+  },
+
+  authorContainer:{
+  },
+
+  authorText:{
+    fontSize:10
+  },
+
+  itemContainer:{
+
+  },
+
+  messageText:{
+    fontSize: 18
+  }
+
+
+})
