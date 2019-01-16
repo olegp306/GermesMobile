c0d39655d4a0fb7772eed6aa1d372fe672e9891b
oleg <olegp306@gmail.com>
2019-01-16 16:15:55 +0300

    add currentUser info and action and reducers for that

---
+++ app/chat/currentUser/actions.js
@@ -0,0 +1,42 @@
+
+ import api from '../../middleware/api'
+ import {keyBy} from 'lodash'
+// https://medium.com/@kylpo/redux-best-practices-eef55a20cc72
+// action name: <NOUN>_<VERB>
+// action creator name: <verb><Noun>
+// selector name: get<Noun>
+ export const CURRENTUSER_GET='CURRENTUSER_GET';
+ export const CURRENTUSER_GET_SUCCESS='CURRENTUSER_GET_SUCCESS';
+ export const CURRENTUSER_GET_FAIL='CURRENTUSER_GET_FAIL';
+
+ CURRENTUSER_GET,
+ CURRENTUSER_GET_SUCCESS,
+ CURRENTUSER_GET_FAIL
+
+ export function getCurrentUser() {
+    return async(dispatch, getState) => {
+      dispatch({ type: CURRENTUSER_GET});
+        try {
+          api.getCurrentUser()
+          .then(data=>dispatch(getCurrentUserSuccess(data.data)))      
+
+      } catch (error) {
+        dispatch(requestsFetchingError(error));
+        //dispatch({ type: CURRENTUSER_GET_FAIL, error });        
+      }
+    };
+  }
+
+  export function getCurrentUserSuccess(item) {
+    return {
+      type: CURRENTUSER_GET_SUCCESS,
+      payload:item
+    }   
+  }
+
+  export function getCurrentUserFail(error) {
+    return {
+      type: CURRENTUSER_GET_FAIL,
+      payload: error
+    } 
+  }
---
+++ app/chat/currentUser/reducer.js
@@ -0,0 +1,32 @@
+import {
+    CURRENTUSER_GET,
+    CURRENTUSER_GET_SUCCESS,
+    CURRENTUSER_GET_FAIL
+} from './actions.js'
+
+import { Map } from 'immutable'
+
+const initialState =new Map({
+    item:{},        
+    isFetching: false,
+    fetched: false,
+    error: null 
+  });
+
+
+  
+  export default currentUserReducer = (state = initialState, action) => {    
+    switch (action.type){
+
+        case CURRENTUSER_GET:
+            return state.merge({isFetching: true, fetched: false, error: null})
+
+        case CURRENTUSER_GET_SUCCESS:
+            return state.merge({isFetching: false, fetched: true, item: action.payload} )
+            
+        case CURRENTUSER_GET_FAIL:
+            return state.merge({isFetching: false, error: action.payload})
+        
+        default: return state
+    }
+}
