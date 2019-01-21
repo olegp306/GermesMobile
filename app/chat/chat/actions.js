47f5e631b54c5795f217945b440a7a1eeec5bd7d
oleg <olegp306@gmail.com>
2019-01-21 17:17:08 +0300

    add CHAT_REQUEST_NOT_FOUND

--- app/chat/chat/actions.js
+++ app/chat/chat/actions.js
@@ -8,6 +8,7 @@
 
 
  export const CHAT_SET_CURRENT='CHAT_SET_CURRENT';
+ export const CHAT_REQUEST_NOT_FOUND='CHAT_REQUEST_NOT_FOUND';
  
 
  export const CHATS_GET_BY_REQUESTID='CHATS_GET_BY_REQUESTID';
@@ -26,19 +27,19 @@
 //  export const CHAT_POST_SUCCESS='CHAT_POST_SUCCESS';
 //  export const CHAT_POST_FAIL='CHAT_POST_FAIL';
 
-export function getMessages(chatId) {
-  return async(dispatch, getState) => {
-    try {
-      dispatch({ type: MESSAGES_GET, chatId });
+// export function getMessages(chatId) {
+//   return async(dispatch, getState) => {
+//     try {
+//       dispatch({ type: MESSAGES_GET, chatId });
 
-       api.getMessagesByChatId(chatId)
-       .then(data=>dispatch(getMessagesSuccess(data.data)))
+//        api.getMessagesByChatId(chatId)
+//        .then(data=>dispatch(getMessagesSuccess(data.data)))
 
-    } catch (error) {
-      dispatch({ type: MESSAGES_GET_FAIL, error });        
-    }
-  };
-}
+//     } catch (error) {
+//       dispatch({ type: MESSAGES_GET_FAIL, error });        
+//     }
+//   };
+// }
 
 
  export function getChatsByRequestId(requestId) {
@@ -65,6 +66,13 @@ export function getMessages(chatId) {
   export function getChatsByRequestIdFail(error) {
     return {
       type: CHATS_GET_BY_REQUESTID_FAIL,
+      payload: error
+    } 
+  }
+
+  export function notFoundChatRequest(requestId) {
+    return {
+      type: CHAT_REQUEST_NOT_FOUND,
       payload: error
     } 
   }
