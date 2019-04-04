a126f4b5b9c1dd98b700676ea424f5cca8603dcb
oleg <olegp306@gmail.com>
2019-04-04 13:06:36 +0300

    add settings for debugging android

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,6 +1,7 @@
 //npm i --save-dev reactotron-react-nativei
-import Reactotron from 'reactotron-react-native' 
-import { reactotronRedux } from 'reactotron-redux'
+import { NativeModules } from "react-native";
+import Reactotron from "reactotron-react-native";
+import { reactotronRedux } from "reactotron-redux";
 
 // Reactotron
 //   .configure() // controls connection & communication settings
@@ -9,14 +10,12 @@ import { reactotronRedux } from 'reactotron-redux'
 
 let scriptHostname;
 if (__DEV__) {
-    const scriptURL = NativeModules.SourceCode.scriptURL;
-    scriptHostname = scriptURL.split('://')[1].split(':')[0];
+  const scriptURL = NativeModules.SourceCode.scriptURL;
+  scriptHostname = scriptURL.split("://")[1].split(":")[0];
 }
 
-
-const reactotron = Reactotron
-  .configure({host: scriptHostname})
+const reactotron = Reactotron.configure({ host: scriptHostname })
   .use(reactotronRedux()) //  <- here i am!
-  .connect() //Don't forget about me!
+  .connect(); //Don't forget about me!
 
-  export default reactotron
+export default reactotron;
