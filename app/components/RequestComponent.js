f6829aed4df621353b4f58f67eefd347c359e23b
oleg <olegp306@gmail.com>
2019-04-10 11:31:25 +0300

    clean and refuct

--- app/components/RequestComponent.js
+++ app/components/RequestComponent.js
@@ -1,196 +1,201 @@
-import React, { Component } from 'react';
-import { CheckBox } from 'react-native-elements';
-import { 
-    View,
-    Text,
-    StyleSheet ,    
-    TouchableOpacity,
-    ActivityIndicator
- } from 'react-native';
+import React, { Component } from "react";
+import { CheckBox } from "react-native-elements";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
 
-import { Colors } from '../theme';
+import { Colors } from "../theme";
 
 export default class RequestComponent extends Component {
-  
-  _handleShortPress=()=>{
+  _handleShortPress = () => {
     this.props.onShortPressRequest(this.props);
-  }
+  };
 
-  _handleLongPress=()=>{
+  _handleLongPress = () => {
     this.props.onLongPressRequest(this.props.requestId);
-  }
-  
-  _handleOnChangeRequestCheckBox=(requestId)=>{
-    this.props.onChangeRequestCheckBox(this.props.requestId)
-  }
+  };
 
+  _handleOnChangeRequestCheckBox = requestId => {
+    this.props.onChangeRequestCheckBox(this.props.requestId);
+  };
 
-  render() {    
+  render() {
     return (
-      <TouchableOpacity 
+      <TouchableOpacity
         onPress={this._handleShortPress}
         onLongPress={this._handleLongPress}
-                  >
+      >
         {/* <ActivityIndicator size='large' /> */}
-        <View style={ this.props.isSelected==true ? styles.selectedContentContainer: styles.unselectedContentContainer}>
-          <View style={ styles.receiptNumberContainer}>        
-            <Text style={styles.receiptNumber}> {this.props.receiptNumber} </Text>
+        <View
+          style={
+            this.props.isSelected == true
+              ? styles.selectedContentContainer
+              : styles.unselectedContentContainer
+          }
+        >
+          <View style={styles.receiptNumberContainer}>
+            <Text style={styles.receiptNumber}>
+              {" "}
+              {this.props.receiptNumber}{" "}
+            </Text>
           </View>
-          
-          
+
           <View style={styles.middleContainer}>
-            <View style={styles.leftMiddleContainer} >
-              <Text>{this.props.requestNumber + " " + this.props.address}</Text>              
-              <Text>{this.props.transactionParticipant}</Text>    
-              { this.props.notice ? (<Text style={styles.notice}>  {this.props.notice} </Text>) : null }              
+            <View style={styles.leftMiddleContainer}>
+              <Text>{this.props.requestNumber + " " + this.props.address}</Text>
+              <Text>{this.props.transactionParticipant}</Text>
+              {this.props.notice ? (
+                <Text style={styles.notice}> {this.props.notice} </Text>
+              ) : null}
             </View>
 
-            <View style={styles.rightMiddleContainer} >
-              <CheckBox 
-                  onPress={this._handleOnChangeRequestCheckBox}
-                  containerStyle={styles.checkboxContainer}              
-                  checkedColor='green'
-                  checked={this.props.isSelected}                  
-               />
-            
-            {this.props.isUpdating ? (<ActivityIndicator size='large' /> ) : null }
+            <View style={styles.rightMiddleContainer}>
+              <CheckBox
+                onPress={this._handleOnChangeRequestCheckBox}
+                containerStyle={styles.checkboxContainer}
+                checkedColor="green"
+                checked={this.props.isSelected}
+              />
+
+              {this.props.isUpdating ? (
+                <ActivityIndicator size="large" />
+              ) : null}
             </View>
           </View>
-
-          
-          
 
           <View style={styles.bottomStikersContainer}>
             <View style={styles.bottomContainer}>
-              {
-                this.props.isBarcodeExist ?
-                (
-                <View style={styles.stikerContainer}>
-                  <Text >Прочитан баркод </Text>                
+              {this.props.errorText ? (
+                <View style={styles.stikerError}>
+                  {/* <Text> Ошибка: {this.props.errorText} </Text> */}
+                  <Text> Ошибка </Text>
                 </View>
-                ):null
-              }
-              {
-                this.props.isChangeStatusSuccess?
-                (
+              ) : null}
+              {this.props.isBarcodeExist ? (
                 <View style={styles.stikerContainer}>
-                  <Text >Статус ПОЛУЧЕНА </Text>                
+                  <Text>Прочитан баркод </Text>
                 </View>
-                ):null
-              }
+              ) : null}
+              {this.props.isChangeStatusSuccess ? (
+                <View style={styles.stikerContainer}>
+                  <Text>Статус ПОЛУЧЕНА </Text>
+                </View>
+              ) : null}
 
-              {
-                this.props.isStatusPriostanovka?
-                (
+              {this.props.isStatusPriostanovka ? (
                 <View style={styles.stikerPriostanovka}>
-                  <Text >приостановка </Text>                
+                  <Text>приостановка </Text>
                 </View>
-                ):null
-              }                
-
-              </View>
-              
-            </View>                  
+              ) : null}
+            </View>
+          </View>
         </View>
-        <View style={styles.horizontalDivider}></View>
-      
+        <View style={styles.horizontalDivider} />
       </TouchableOpacity>
     );
   }
 }
 const styles = StyleSheet.create({
   unselectedContentContainer: {
-    width: '100 %', 
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    marginBottom:2,
+    width: "100 %",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    marginBottom: 2
     //backgroundColor:Colors.ligth2
   },
- 
+
   selectedContentContainer: {
-    width: '100 %', 
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    marginBottom:2,
-    opacity:0.5
+    width: "100 %",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    marginBottom: 2,
+    opacity: 0.5
   },
 
   checkboxContainer: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 0
   },
-  
-  receiptNumberContainer:{
-    //backgroundColor: Colors.baseBackgroundColor
-    },
-  receiptNumber:{      
-    fontWeight:'500',
-    fontSize: 17,
-    marginLeft: 10,
-    },
 
-  stikerContainer:{     
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    borderRadius: 1,
-    backgroundColor: '#4ef235',
+  receiptNumberContainer: {
+    //backgroundColor: Colors.baseBackgroundColor
   },
-  
-  stikerPriostanovka:{
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+  receiptNumber: {
+    fontWeight: "500",
+    fontSize: 17,
+    marginLeft: 10
+  },
+
+  stikerContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 1,
+    backgroundColor: "#4ef235"
+  },
+
+  stikerPriostanovka: {
+    flexDirection: "row",
+    alignItems: "flex-start",
     borderRadius: 1,
     backgroundColor: Colors.statusPriostanovkaBackgroundColor
   },
 
-    
-  notice:{
-      fontStyle:'italic'
+  stikerError: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 1,
+    backgroundColor: Colors.warningBackgroundColor
   },
 
-  middleContainer:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
-    marginLeft: 10,
-  },
-  
-  leftMiddleContainer:{
-    width: '85%'
+  notice: {
+    fontStyle: "italic"
   },
 
-  rightMiddleContainer:{
-    width: '15%',
-    alignItems: 'center',
+  middleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    marginLeft: 10
+  },
+
+  leftMiddleContainer: {
+    width: "85%"
+  },
+
+  rightMiddleContainer: {
+    width: "15%",
+    alignItems: "center"
     //backgroundColor:Colors.ligth2
   },
-    
-  bottomContainer:{
+
+  bottomContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'stretch'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch"
   },
 
-  bottomStikersContainer:{
+  bottomStikersContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'stretch'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch"
   },
 
-    horizontalDivider: {
-        width: '100%',
-        height: 1,
-        backgroundColor: "#6E6E6E",
-        // justifyContent: 'space-between',
-    },
-    checkbox:{
-     
-      // borderColor:Colors.actionItemColor,
-      // color:Colors.actionItemColor
-    }
+  horizontalDivider: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "#6E6E6E"
+    // justifyContent: 'space-between',
+  },
+  checkbox: {
+    // borderColor:Colors.actionItemColor,
+    // color:Colors.actionItemColor
+  }
 });
-
