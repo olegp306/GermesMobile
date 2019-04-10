77e630991f128b741154141ff2d7cd320b959eed
oleg <olegp306@gmail.com>
2019-04-10 12:36:03 +0300

    add image info in message data

--- app/camera/ImagePickerComponent.js
+++ app/camera/ImagePickerComponent.js
@@ -117,6 +117,7 @@ export default class ImagePickerComponent extends Component {
 
     let message = {
       type: 2768777880000, //картинка
+      image: image,
       text: image.uri,
       userId: currentUserId,
       chatId: currentChatId,
