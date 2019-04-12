d1e23839c27eda11111057e451e859861bd998de
oleg <olegp306@gmail.com>
2019-04-12 13:03:29 +0300

    separate method for text and file mesaage

--- app/chat/message/actions.js
+++ app/chat/message/actions.js
@@ -2,8 +2,9 @@ import api from "../../middleware/api";
 import { keyBy } from "lodash";
 import {
   addNewMessage,
-  getMessages as getChatMessagesByChatId
+  getMessages as getChatMessagesByChatId, 
 } from "../messages/actions";
+
 // https://medium.com/@kylpo/redux-best-practices-eef55a20cc72
 // action name: <NOUN>_<VERB>
 // action creator name: <verb><Noun>
@@ -14,23 +15,55 @@ export const MESSAGE_POST_FAIL = "MESSAGE_POST_FAIL";
 
 export function postMessage(message) {
   return async (dispatch, getState) => {
-    //dispatch(addNewMessage(message))
     dispatch({ type: MESSAGE_POST });
     try {
-      if (message.type == 2768777880000) {
-        //картинка)
-        api.postFile(message).then(data => {
+      api.postMessage(message).then(data => {
+        dispatch(postMessageSuccess(data.data));
+        //todo add new message in MESSAGES
+        //dispatch(addNewMessage(data.data))
+      });
+    } catch (error) {
+      dispatch({ type: MESSAGE_POST_FAIL, error });
+    }
+  };
+}
+
+export function postFileMessage(fileMessage) {
+  return async (dispatch, getState) => {
+    dispatch({ type: MESSAGE_POST });
+    try {
+      //отправляем новый файл на сервер
+      api.postFile(fileMessage.file).then(response => {
+        // let fileMessage = {
+        //   type: 2768654243000, //картинка
+        //   image: image,
+        //   text: "file message",
+        //   userId: currentUserId,
+        //   chatId: currentChatId,
+        //   tempFrontId: image.uri + new Date(),
+        //   creationDate: new Date()
+        // };
+        //забераем из data info о загруженной картинке и
+        //response.data.id
+        //добавляем данные в сообщение о картинке
+        //message.file.Id=response.data[0].id
+
+        //message.type= 2768842251000 // файл
+        //type: 2768777880000, //картинка
+
+        imageId = response.data[0].id;
+        fileMessage.fileId = response.data[0].id;
+        //fileMessage.type = 2768654243000; //картинка
+        //fileMessage.text = "сообщение типа картинка";
+
+        api.postMessage(fileMessage).then(data => {
           dispatch(postMessageSuccess(data.data));
-          //todo add new message in MESSAGES
-          //dispatch(addNewMessage(data.data))
         });
-      } else {
-        api.postMessage(message).then(data => {
-          dispatch(postMessageSuccess(data.data));
-          //todo add new message in MESSAGES
-          //dispatch(addNewMessage(data.data))
-        });
-      }
+        //dispatch(postMessageSuccess(data.data));
+
+        //todo add new message in MESSAGES
+        //dispatch(addNewMessage(data.data))
+      });
     } catch (error) {
       dispatch({ type: MESSAGE_POST_FAIL, error });
     }
