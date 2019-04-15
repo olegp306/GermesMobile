82714f779a27675a14e293a2f2c02d91ee426677
oleg <olegp306@gmail.com>
2019-04-15 15:03:29 +0300

    add 2 new version of layout screen for customer

--- app/containers/CustomerFirstScreen.js
+++ app/containers/CustomerFirstScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { Images } from "../theme/";
 import {
   Caption,
   Avatar,
@@ -7,7 +8,9 @@ import {
   Card,
   Title,
   Paragraph,
-  Badge
+  Badge,
+  IconButton,
+  Text
 } from "react-native-paper";
 
 export default class CustomerFirstScreeen extends Component {
@@ -18,7 +21,8 @@ export default class CustomerFirstScreeen extends Component {
 
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.contentContainer}>
+      // <ScrollView contentContainerStyle={styles.contentContainer}>
+      <View>
         <View style={{ height: "1%", width: "100%" }} />
         {/* Сведения о пользователе */}
         <View style={{ alignItems: "center" }}>
@@ -26,7 +30,9 @@ export default class CustomerFirstScreeen extends Component {
             <Card.Title
               title="Петрова Мария Ивановна"
               subtitle="Центр-Инвест"
-              left={props => <Avatar.Icon {...props} icon="person" />}
+              left={props => (
+                <Avatar.Image size={50} source={Images.avatarExample} />
+              )}
               // verified-user
             />
             {/* <Card.Content style={styles.cardContent}>
@@ -35,154 +41,225 @@ export default class CustomerFirstScreeen extends Component {
             </Card.Content> */}
           </Card>
         </View>
-
-        <View style={{ height: "12%", width: "100%" }} />
-
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center"
-            //alignItems: "center",
-          }}
-        >
-          {/* Замечания */}
-          {/* <View style={{ alignItems: "center" }}> */}
-          <Card style={{ width: "46%" }}>
-            <Card.Title
-              title="Замечания"
-              subtitle="Заявки Открытыми замечания"
-              elevation={3}
-              left={props => (
-                <Avatar.Icon {...props} icon="notifications-active" />
-              )}
-            />
-          </Card>
-          {/* </View> */}
-
-          <View style={{ width: "2%" }} />
-
-          {/* Приостановка */}
-          {/* <View style={{ alignItems: "center" }}> */}
-          <Card style={{ width: "46%" }}>
-            <Card.Title
-              title="Приостановка"
-              subtitle="Приостановка"
-              elevation={3}
-              left={props => <Avatar.Icon {...props} icon="pause" />}
-            />
-          </Card>
-
-          {/* <Card style={{ width: "40%" }}>
+        <View style={{ height: "1%", width: "100%" }} />
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+        
+          <View style={{ height: "5%", width: "100%" }} />
+          <Text> Вариант №1 </Text>
+          {/* Получено Замечания */}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center"
+              //alignItems: "center",
+            }}
+          >
+            {/* Получено */}
+            <Card style={{ width: "46%" }}>
+              <Card.Title title="Получено" elevation={3} />
+              <Text style={{ color: "red", textAlign: "center" }}>13 шт </Text>
+            </Card>
+            {/* Замечания */}
+            <View style={{ width: "2%" }} />
+            <Card style={{ width: "46%" }}>
+              <Card.Title
+                title="Замечания"
+                // subtitle="Открытыми замечания"
+                elevation={3}
+              />
+              <Text style={{ color: "red", textAlign: "center" }}>3 шт </Text>
+            </Card>
+          </View>
+          <View style={{ height: "1%", width: "100%" }} />
+          {/* Приостановка Сдана */}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center"
+              //alignItems: "center",
+            }}
+          >
+            {/* Получено */}
+            <Card style={{ width: "46%" }}>
               <Card.Title
                 title="Приостановка"
-                // subtitle="Приостановка"
+                // subtitle="статус приостановка"
                 elevation={3}
-                left={props => <Avatar.Icon {...props} icon="pause" />}
               />
-            </Card> */}
-          {/* </View> */}
-        </View>
-        <View style={{ height: "2%", width: "100%" }} />
-        <View style={{ alignItems: "center" }}>
-          <Card style={{ width: "95%" }}>
-            <Card.Title
-              title=" Получено (сегодня)"
-              subtitle="Заявки которые сегодня получены"
+              <Text style={{ color: "red", textAlign: "center" }}>1 шт </Text>
+            </Card>
+            {/* Замечания */}
+            <View style={{ width: "2%" }} />
+            <Card style={{ width: "46%" }}>
+              <Card.Title title="Сдана" 
+              // subtitle="статус сдана" 
               elevation={3}
-              left={props => <Avatar.Icon {...props} icon="today" />}
-              // verified-user
-            />
-            {/* <Card.Content style={styles.cardContent}>
-              <Title>Петрова Мария Ивановна</Title>
-              <Paragraph>Центр-Инвест. Роль - 'Заказчик'</Paragraph>
-            </Card.Content> */}
-            {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
-          </Card>
-        </View>
+               />
+              <Text style={{ color: "red", textAlign: "center" }}>12 шт </Text>
+            </Card>
+          </View>
 
-        <View style={{ height: "2%", width: "100%" }} />
-        <View style={{ alignItems: "center" }}>
-          <Card style={{ width: "95%" }}>
-            <Card.Title
-              title="Сдана"
-              // subtitle="Заявки которые сегодня получены"
-              elevation={3}
-              left={props => <Avatar.Icon {...props} icon="update" />}
-              // verified-user
-            />
-            <Card.Content style={styles.cardContent}>
-              <View style={styles.rowContainer}>
-                <Paragraph>27.03.2018</Paragraph>
-                <Paragraph>3 шт.</Paragraph>
-              </View>
+          <View style={{ height: "2%", width: "100%" }} />
+          <Text> Вариант №2 </Text>
 
-              <View style={styles.rowContainer}>
-                <Paragraph>28.03.2018</Paragraph>
-                <Paragraph>9 шт.</Paragraph>
-              </View>
+          <View style={{ alignItems: "center" }}>
+            <Card style={{ width: "95%" }}>
+              <Card.Title
+                title=" Получены сегодня"
+                // subtitle="я получены"
+                elevation={3}
+                right={props => <Text style={{ color: "red" }}>13 шт </Text>}
+                // <Badge {...props} children={3} size={30} />
+              />
+            </Card>
+          </View>
+          <View style={{ height: "1%", width: "100%" }} />
 
-              <View style={styles.rowContainer}>
-                <Paragraph>29.03.2018</Paragraph>
-                <Paragraph>22 шт.</Paragraph>
-              </View>
-            </Card.Content>
+          <View style={{ alignItems: "center" }}>
+            <Card style={{ width: "95%" }}>
+              <Card.Title
+                title=" Замечания"
+                // subtitle="Заявки которые сегодня получены"
+                elevation={3}
+                right={props => <Text style={{ color: "red" }}>3 шт </Text>}
+              />
+            </Card>
+          </View>
+          <View style={{ height: "1%", width: "100%" }} />
+          <View style={{ alignItems: "center" }}>
+            <Card style={{ width: "95%" }}>
+              <Card.Title
+                title=" Приостановка"
+                elevation={3}
+                // right={props => <Badge {...props} children={3} size={30} />}
+                right={props => <Text style={{ color: "red" }}>1 шт </Text>}
+              />
+            </Card>
+          </View>
+          <View style={{ height: "1%", width: "100%" }} />
+          <View style={{ alignItems: "center" }}>
+            <Card style={{ width: "95%" }}>
+              <Card.Title
+                title=" Сдана"
+                // subtitle="Заявки которые сегодня получены"
+                elevation={3}
+                right={props => <Text style={{ color: "red" }}>12 шт </Text>}
+              />
+            </Card>
+          </View>
 
-            {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
-          </Card>
-        </View>
+          <View style={{ height: "2%", width: "100%" }} />
+          <View style={{ alignItems: "center" }}>
+            <Card style={{ width: "95%" }}>
+              <Card.Title
+                title="Статистика сданных дел"
+                // subtitle="Заявки которые сегодня получены"
+                elevation={3}
+                left={props => <Avatar.Icon {...props} icon="update" />}
+                // verified-user
+              />
+              <Card.Content style={styles.cardContent}>
+                <View style={styles.rowContainer}>
+                  <Paragraph>27.03.2018</Paragraph>
+                  <Paragraph>3 шт.</Paragraph>
+                </View>
 
-        <View style={{ height: "2%", width: "100%" }} />
+                <View style={styles.rowContainer}>
+                  <Paragraph>28.03.2018</Paragraph>
+                  <Paragraph>9 шт.</Paragraph>
+                </View>
 
-        <View style={{ alignItems: "center" }}>
-          <Card style={{ width: "95%" }}>
-            <Card.Title
-              title=" Сдана"
-              // subtitle="Заявки которые сегодня получены"
-              elevation={3}
-              left={props => <Avatar.Icon {...props} icon="update" />}
-              // verified-user
-            />
-            <Card.Content style={styles.cardContent}>
-              <View style={styles.rowContainer}>
-                <Paragraph>27.03.2018</Paragraph>
-                <Paragraph>3 шт.</Paragraph>
-              </View>
+                <View style={styles.rowContainer}>
+                  <Paragraph>29.03.2018</Paragraph>
+                  <Paragraph>22 шт.</Paragraph>
+                </View>
+              </Card.Content>
 
-              <View style={styles.rowContainer}>
-                <Paragraph>28.03.2018</Paragraph>
-                <Paragraph>9 шт.</Paragraph>
-              </View>
+              {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
+            </Card>
+          </View>
 
-              <View style={styles.rowContainer}>
-                <Paragraph>29.03.2018</Paragraph>
-                <Paragraph>22 шт.</Paragraph>
-              </View>
-            </Card.Content>
+          <View style={{ height: "2%", width: "100%" }} />
+          <Text>Тест блок для проверки скролла вниз</Text>
+          <View style={{ alignItems: "center" }}>
+            <Card style={{ width: "95%" }}>
+              <Card.Title
+                title="Тест блок для проверки скролва вниз"
+                elevation={3}
+                left={props => <Avatar.Icon {...props} icon="update" />}
+                // verified-user
+              />
+              <Card.Content style={styles.cardContent}>
+                <View style={styles.rowContainer}>
+                  <Paragraph>27.03.2018</Paragraph>
+                  <Paragraph>3 шт.</Paragraph>
+                </View>
 
-            {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
-          </Card>
-        </View>
-        <View style={{ height: "12%", width: "100%" }} />
-      </ScrollView>
+                <View style={styles.rowContainer}>
+                  <Paragraph>28.03.2018</Paragraph>
+                  <Paragraph>9 шт.</Paragraph>
+                </View>
+
+                <View style={styles.rowContainer}>
+                  <Paragraph>29.03.2018</Paragraph>
+                  <Paragraph>22 шт.</Paragraph>
+                </View>
+              </Card.Content>
+
+              {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
+            </Card>
+          </View>
+          <View style={{ height: "2%", width: "100%" }} />
+          <View style={{ alignItems: "center" }}>
+            <Card style={{ width: "95%" }}>
+              <Card.Title
+                title="Тест блок 2 для проверки скролва вниз"                
+                elevation={3}
+                left={props => <Avatar.Icon {...props} icon="update" />}
+                // verified-user
+              />
+              <Card.Content style={styles.cardContent}>
+                <View style={styles.rowContainer}>
+                  <Paragraph>27.03.2018</Paragraph>
+                  <Paragraph>3 шт.</Paragraph>
+                </View>
+
+                <View style={styles.rowContainer}>
+                  <Paragraph>28.03.2018</Paragraph>
+                  <Paragraph>9 шт.</Paragraph>
+                </View>
+
+                <View style={styles.rowContainer}>
+                  <Paragraph>29.03.2018</Paragraph>
+                  <Paragraph>22 шт.</Paragraph>
+                </View>
+              </Card.Content>
+
+              {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
+            </Card>
+          </View>
+
+          <View style={{ height: "12%", width: "100%" }} />
+        </ScrollView>
+      </View>
     );
   }
 }
 const styles = StyleSheet.create({
   contentContainer: {
-    paddingBottom: 120
+    paddingBottom: 250
   },
 
   rowContainer: {
     flexDirection: "row",
-    width:'50%',
+    width: "50%",
     justifyContent: "space-between",
-    alignItems: "center",
+    alignItems: "center"
     // backgroundColor:"steelblue"
   },
 
-  cardContent:{
-    alignItems: 'center',
+  cardContent: {
+    alignItems: "center"
     // backgroundColor:"yellow"
-  
   }
 });
