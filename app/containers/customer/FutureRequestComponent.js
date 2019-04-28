90234783883d41147da44248cd2ae1f5be40ad93
oleg <olegp306@gmail.com>
2019-04-28 11:35:33 +0300

    imp futureRequestsComponent

--- app/containers/customer/FutureRequestComponent.js
+++ app/containers/customer/FutureRequestComponent.js
@@ -1,66 +1,153 @@
-import React, { Component } from 'react';
-import { View,StyleSheet } from 'react-native';
-import {
-    Avatar,
-    Card,
-    Paragraph,
-    Text,
-    Button,
-    Badge
-  } from "react-native-paper";
+import React, { Component } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import Colors from "../../theme/Colors";
+import _ from "lodash";
 
-export default class componentName extends Component {
+import {
+  Avatar,
+  Card,
+  Paragraph,
+  Text,
+  Button,
+  Badge
+} from "react-native-paper";
+
+const statusList = {
+  "95485390000": {
+    id: "95485390000",
+    name: "сдана"
+  },
+  "97670516000": {
+    id: "97670516000",
+    name: "приостановлена"
+  },
+  "95486490000": {
+    id: "95486490000",
+    name: "получена"
+  }
+};
+
+export default class FutureRequestComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
+  _getStringDate = fullDate => {
+    return (
+      fullDate.getDate() +
+      "." +
+      (fullDate.getMonth() + 1 < 10
+        ? "0" + (fullDate.getMonth() + 1)
+        : fullDate.getMonth()) +
+      "." +
+      fullDate.getFullYear()
+    );
+  };
+
+  _getFutureContainerContent = recieviedRequests => {
+    const recieviedArOrdered = recieviedRequests.sort(function compare(a, b) {
+      var dateA = new Date(a.fromRegistrationPlanDate);
+      var dateB = new Date(b.fromRegistrationPlanDate);
+      return dateA - dateB;
+    });
+
+    let contentList = {};
+    let nextDate = {};
+    recieviedArOrdered.forEach(el => {
+      let date = new Date(el.fromRegistrationPlanDate);
+
+      if (this._getStringDate(date) == nextDate) {
+        contentList[date].count = contentList[date].count + 1;
+      } else {
+        nextDate = this._getStringDate(date);
+        contentList[date] = { date: nextDate, fullDate: date, count: 1 };
+      }
+    });
+    const contentAr = _.values(contentList).sort(function(a, b) {
+      return new Date(b.date) - new Date(a.date);
+    });
+
+    return contentAr;
+  };
 
   render() {
+    const { isFetching, items, feched } = this.props.requests;
+    const requestsAr = _.values(items);
+    const recieviedRequests = requestsAr.filter(item => {
+      return item.statusId == 95486490000;
+    });
+
+    const contentAr = this._getFutureContainerContent(recieviedRequests);
+
+    let contentItems = contentAr.map((item, index) => {
+      return (
+        <View style={styles.rowContainer}>
+          <Text style={styles.itemText}>{item.date}</Text>
+          <Text style={styles.itemText}>{item.count}</Text>
+        </View>
+      );
+    });
+
     return (
-        <View style={{ alignItems: "center" }}>
-        <Card style={{ width: "95%" }}>
-          <Card.Title
-            title="Не работает Статистика сданных дел"
-            subtitle="Заявки которые будут скоро получены"
-            elevation={3}
-            left={props => <Avatar.Icon {...props} icon="update" />}
-            // verified-user
-          />
-          <Card.Content style={styles.cardContent}>
-            <View style={styles.rowContainer}>
-              <Paragraph>27.03.2018</Paragraph>
-              <Paragraph>3 шт.</Paragraph>
-            </View>
+      <TouchableOpacity
+        style={styles.smallContainerWithShadowStyle}
+        onPress={this.props.onPress}
+      >
+        <Text style={styles.buttonText}>ПЛАН ПОЛУЧЕНИЯ ЗАЯВОК</Text>
 
-            <View style={styles.rowContainer}>
-              <Paragraph>28.03.2018</Paragraph>
-              <Paragraph>9 шт.</Paragraph>
-            </View>
-
-            <View style={styles.rowContainer}>
-              <Paragraph>29.03.2018</Paragraph>
-              <Paragraph>22 шт.</Paragraph>
-            </View>
-          </Card.Content>
-        </Card>
-      </View>
+        <Text style={styles.buttonSmallText}>
+          Заявки которые скоро будут получены
+        </Text>
+        <View style={{alignItems: 'center',}}> {contentItems}</View>>
+       
+      </TouchableOpacity>
     );
   }
 }
 const styles = StyleSheet.create({
-  
-  
-    cardContent: {
-      alignItems: "center"
-      // backgroundColor:"yellow"
-    },
-    rowContainer: {
-        flexDirection: "row",
-        width: "50%",
-        justifyContent: "space-between",
-        alignItems: "center"
-        // backgroundColor:"steelblue"
-      },
-  });
-  
+  cardContent: {
+    alignItems: "center"
+    // backgroundColor:"yellow"
+  },
+  rowContainer: {
+    flexDirection: "row",
+    width: "50%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: '2%',
+    // backgroundColor:"steelblue"
+  },
+  smallContainerWithShadowStyle: {
+    width: "96%",
+    minHeight: 100,
+    justifyContent: "space-evenly",
+    borderRadius: 5,
+    backgroundColor: Colors.navigatorBackgroudColor,
+    shadowOpacity: 0.75,
+    shadowRadius: 3,
+    shadowColor: "gray",
+    shadowOffset: { height: 0, width: 0 }
+  },
+  buttonText: {
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "400",
+    color: "white"
+  },
+  buttonSmallText: {
+    fontSize: 14,
+    //fontWeight: "400",
+    color: "white",
+    textAlign: "center",
+    alignItems: "flex-end",
+    marginRight: 10
+  },
+  itemText: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: "white",
+    textAlign: "center",
+    alignItems: "flex-end",
+    marginRight: 10
+  }
+});
